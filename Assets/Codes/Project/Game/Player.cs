eaef33de81af4bff6ccf9261e14fcc747cbc398c
qzqzqqz <qizhao2222@gmail.com>
2024-03-08 15:23:32 +0800

    平滑移动

--- Assets/Codes/Project/Game/Player.cs
+++ Assets/Codes/Project/Game/Player.cs
@@ -6,9 +6,11 @@ namespace PlatformShoot
 {
     public class Player : MonoBehaviour, IController
     {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
-        private Rigidbody2D mRig;
+        private Rigidbody2D _mRig;
         private BoxCollider2D _mCollBox;
         private LayerMask _mGroundLayerMask;
+        private float _mAccDelta = 0.6f;
+        private float _mDecDelta = 0.9f;
         private float _mGroundMoveSpeed = 5f;
         private float _mJumpForce = 12f;
         private bool _mJumpInput;
@@ -17,7 +19,7 @@ namespace PlatformShoot
         // Start is called before the first frame update
         private void Start()
         {
-            mRig = GetComponent<Rigidbody2D>();
+            _mRig = GetComponent<Rigidbody2D>();
             _mCollBox = GetComponentInChildren<BoxCollider2D>();
             _mGroundLayerMask = LayerMask.GetMask("Ground");
             this.GetSystem<ICameraSystem>().SetTarget(transform);
@@ -45,10 +47,26 @@ namespace PlatformShoot
                 }
             }
             //判断角色转向
-            if ((Input.GetAxisRaw("Horizontal") != 0) && (Input.GetAxisRaw("Horizontal") != _mFaceDir))
+            float h = Input.GetAxisRaw("Horizontal");
+            if ((h != 0) && (h != _mFaceDir))
             {
                 _mFaceDir = -_mFaceDir;
                 transform.Rotate(0, 180, 0);
+            }
+        }
+        
+        //平滑移动
+        private void SmoothMove(float input)
+        {
+            if (input != 0)
+            {
+                _mRig.velocity =
+                    new Vector2(Mathf.Clamp(_mRig.velocity.x + input * _mAccDelta, -_mGroundMoveSpeed, _mGroundMoveSpeed),
+                        _mRig.velocity.y);
+            }
+            else
+            {
+                _mRig.velocity = new Vector2(Mathf.MoveTowards(_mRig.velocity.x, 0, _mDecDelta), _mRig.velocity.y);
             }
         }
 
@@ -64,9 +82,11 @@ namespace PlatformShoot
             if (_mJumpInput)
             {
                 _mJumpInput = false;
-                mRig.velocity = new Vector2(mRig.velocity.x, _mJumpForce);
+                _mRig.velocity = new Vector2(_mRig.velocity.x, _mJumpForce);
             }
-            mRig.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * _mGroundMoveSpeed, mRig.velocity.y);
+            float h = Input.GetAxisRaw("Horizontal");
+            SmoothMove(h);
+            // mRig.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * _mGroundMoveSpeed, mRig.velocity.y);
         }
 
         private void LateUpdate()
